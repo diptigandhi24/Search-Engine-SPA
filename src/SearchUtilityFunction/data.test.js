785d230b135992586ad0d0f58928ad9ff6de51bd
diptigandhi24 <gandhidipti.s@gmail.com>
2020-08-28 18:39:35 +0530

    Need better sepration of function and data

--- src/SearchUtilityFunction/data.test.js
+++
@@ -1,5 +0,0 @@
-import { summariesIndexing } from "./data";
-
-test("Ckecking the indexing datastructure with input string returns the correct books of list", () => {
-  expect(summariesIndexing.get("life")["id"]).toBe([1, 2, 3, 4]);
-});
