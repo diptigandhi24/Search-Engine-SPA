6c7b75717ef67917bba7b014eeffddd4e858c63d
diptigandhi24 <gandhidipti.s@gmail.com>
2020-08-31 10:53:27 +0530

    I used console for testing this function

--- src/SearchUtilityFunction/searchFunction.test.js
+++ src/SearchUtilityFunction/searchFunction.test.js
@@ -1,5 +1,15 @@
-import { searchBookList } from "./searchFunction";
+import { searchForBooks } from "./searchFunction";
+// import { summariesList, bookTitlesList, bookAuthorsList } from "./data";
 
-test("check whether user input is valid query", () => {
-  expect(searchBookList("patriachy")).toBe(true);
+let queryList = "hello! world life fuck";
+test("get the books with respect to users input", () => {
+  //   expect(searchBookList("patriachy")).toBe(undefined);
+  // expect(searchForBooks("the world Horror life")).toBe(true);
+  expect(searchForBooks(queryList, 5)).toStrictEqual(["word"]);
 });
+
+// test("Filter the stop words", () => {
+//   expect(filterStopWords("world is beautiful has been in the school")).toBe(
+//     "world beautiful school"
+//   );
+// });
