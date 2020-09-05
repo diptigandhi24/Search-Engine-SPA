9f1f43fbaa6b77fe5e68d9cf838641a087dfedf9
diptigandhi24 <gandhidipti.s@gmail.com>
2020-09-05 19:29:35 +0530

    intitaize the Map and create the vairbale inside the test function for better test

--- src/SearchUtilityFunction/Index/SummariesKeywordIndex.test.js
+++ src/SearchUtilityFunction/Index/SummariesKeywordIndex.test.js
@@ -1,21 +1,22 @@
 import { summariesKeywordsIndex } from "./SummariesKeywordIndex";
 
-let summariesCollection = [
-  {
-    id: 0,
-    summary: "Hello!! World Hello *",
-  },
-  {
-    id: 1,
-    summary: "Beaituful 10X 1) beaituful Hello",
-  },
-];
-let index = new Map();
-index.set("Hello", { id: [0, 1] });
-index.set("World", { id: [0] });
-index.set("Beaituful", { id: [1] });
-index.set("10X", { id: [1] });
-index.set("beaituful", { id: [1] });
-test("console.log the Correct list of keywords", () => {
+test("build index of unique keywords with the list of summary they appear in", () => {
+  let summariesCollection = [
+    {
+      id: 0,
+      summary: "Hello!! World Hello *",
+    },
+    {
+      id: 1,
+      summary: "Beaituful 10X 1) beaituful Hello",
+    },
+  ];
+  let index = new Map([
+    ["Hello", { id: [0, 1] }],
+    ["World", { id: [0] }],
+    ["Beaituful", { id: [1] }],
+    ["10X", { id: [1] }],
+    ["beaituful", { id: [1] }],
+  ]);
   expect(summariesKeywordsIndex(summariesCollection)).toEqual(index);
 });
