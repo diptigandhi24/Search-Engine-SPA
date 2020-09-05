85a9285a526224a353ad898e7f4b713e7f4327c7
diptigandhi24 <gandhidipti.s@gmail.com>
2020-09-05 18:38:41 +0530

    Search Books is not one function with its private function, so it becomes easy to follow the code

--- src/SearchUtilityFunction/searchFunction.js
+++ src/SearchUtilityFunction/searchFunction.js
@@ -3,25 +3,55 @@ import indexHashMap from "./Index/SummariesKeywordIndex.js";
 import stopWordsList from "./Index/stopWordsList.js";
 import accumulateCommonBooks from "./accumulateCommonBooks";
 
-export function filterStopWords(userQuery) {
-  const regex = /[!“”#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
-  let queryLists = userQuery.replace(regex, "").split(" ");
+export default function searchForBooks(userQuery, noOfBooks) {
+  let queryList = filterStopWords(userQuery);
+  let allSearchResults = new Map();
+  let commonBooksCounter = new Map();
+
+  //get all the books with respect to each query word
+  queryList.forEach((word) =>
+    getTheBooksBasedOnEachQueryWord(allSearchResults, word)
+  );
+  // count the books that has appear  more than once with respect to each query word
+  allSearchResults.forEach((booksId) =>
+    accumulateCommonBooks(booksId, commonBooksCounter)
+  );
+
+  let bookList = sortBooksByPriority(commonBooksCounter);
+  let searchResults = bookList.splice(0, noOfBooks);
+  let displayBooksTitle = [];
+  searchResults.forEach((id) => {
+    displayBooksTitle.push({
+      bookTitle: bookTitlesList[id.bookId],
+      bookId: id.bookId,
+    });
+  });
+
+  return displayBooksTitle.length === 0 ? [] : displayBooksTitle;
+}
+
+function filterStopWords(userQuery) {
+  const unwantedCharsRegex = /[!“”#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+  let queryLists = userQuery.replace(unwantedCharsRegex, "").split(" ");
   let filterQueryList = queryLists.filter(
     (word) => !stopWordsList.has(word) && indexHashMap.has(word)
   );
   return filterQueryList;
 }
 
-function getTheBooksbasedOnEachQueryWord(allsearchresults, queryWord) {
+export function getBookDetails(id) {
+  return {
+    bookAuthor: bookAuthorsList[id].author,
+    bookSummary: summariesList[id].summary,
+  };
+}
+
+function getTheBooksBasedOnEachQueryWord(allsearchresults, queryWord) {
   allsearchresults.set(queryWord, indexHashMap.get(queryWord)["id"]);
 }
 
-function getThebookCounts(booksId, commonBooksCounter) {
-  accumulateCommonBooks(booksId, commonBooksCounter);
-}
-
 //sort the books with high-low counters
-function sortHighPriorityBook(map) {
+function sortBooksByPriority(map) {
   //create an array of book object with counter as key and book id as a value
   let bookList = Array.from(map, ([bookId, bookCounter]) => ({
     bookCounter,
@@ -40,38 +70,4 @@ function sortHighPriorityBook(map) {
   }
   bookList.sort(compare);
   return bookList;
-}
-
-export default function searchForBooks(userQuery, noOfBooks) {
-  let queryList = filterStopWords(userQuery);
-  let allsearchedresults = new Map();
-  let commonBooksCounter = new Map();
-
-  //get all the books with respect to each query word
-  queryList.forEach((word) =>
-    getTheBooksbasedOnEachQueryWord(allsearchedresults, word)
-  );
-  // count the books that has appear  more than once with respect to each query word
-  allsearchedresults.forEach((booksId) =>
-    getThebookCounts(booksId, commonBooksCounter)
-  );
-
-  let bookList = sortHighPriorityBook(commonBooksCounter);
-  let searchResults = bookList.splice(0, noOfBooks);
-  let displayBooksTitle = [];
-  searchResults.forEach((id) => {
-    displayBooksTitle.push({
-      bookTitle: bookTitlesList[id.bookId],
-      bookId: id.bookId,
-    });
-  });
-
-  return displayBooksTitle.length === 0 ? [] : displayBooksTitle;
-}
-
-export function getBookDetails(id) {
-  return {
-    bookAuthor: bookAuthorsList[id].author,
-    bookSummary: summariesList[id].summary,
-  };
 }
