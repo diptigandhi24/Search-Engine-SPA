2c574615d0fb6915505cc369034739fa1fd46c17
diptigandhi24 <gandhidipti.s@gmail.com>
2020-09-08 18:24:14 +0530

    changes the file name

--- src/Components/SearchBooks/autoComplete.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function OptionsList(props) {
-  return (
-    <li key={props.id} id={props.id} onClick={props.onClick}>
-      {props.children}
-    </li>
-  );
-}
